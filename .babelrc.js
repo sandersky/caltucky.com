d440e1b55cad52a796c839771ed054e09ee771ef
Matthew Dahl <dahl.matthew@gmail.com>
2018-01-06 19:35:19 -0800

    Move auto-CSS modules to plugin

--- .babelrc.js
+++ .babelrc.js
@@ -2,7 +2,7 @@ module.exports = {
   "plugins": [
     "@babel/plugin-proposal-class-properties",
     "@babel/plugin-proposal-object-rest-spread",
-    "./plugins/babel-plugin-auto-css-modules",
+    "babel-plugin-auto-css-modules",
   ],
   "presets": [
     [
